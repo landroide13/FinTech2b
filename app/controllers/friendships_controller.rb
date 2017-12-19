6df6bd00c8c27b79e91012c95a0d7165a726e605
landroide13 <lazaro02@hotmail.com>
2017-12-20 12:22:03 +1300

    Ended ?

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,11 @@
+class FriendshipsController < ApplicationController
+
+  def destroy
+    @friendship = current_user.friendships.where(friend_id: params[:id]).first
+    @friendship.destroy
+    flash[:notice] = "Friend successfully deleted.."
+    redirect_to my_friends_path 
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,13 @@ Rails.application.routes.draw do
   get 'my_portfolio' => 'users#my_portfolio'
   get 'search_stocks' => 'stocks#search'
 
-  resources :user_stocks, only:[:create, :destroy] 
+  resources :user_stocks, only:[:create, :destroy]
+  resources :users, only:[:show] 
+  resources :friendships
 
   get 'my_friends' => 'users#my_friends'
+  get 'search_friends' => 'users#search'
+  post 'add_friend' => 'users#add_friend'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
